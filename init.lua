020e4f8907be1fc491e4b90a7ea84472ccac8f82
Marcin Iwaniak <miwaniak@betacom.com.pl>
2022-05-10 15:27:11 +0200

    Add keymap for hop one char worl find

--- init.lua
+++ init.lua
@@ -222,6 +222,7 @@ local config = {
     vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
     vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
     vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
+    vim.keymap.set("n", "æ", ":HopChar1MW<cr>")
     -- Set autocommands
     vim.api.nvim_create_augroup("packer_conf", { clear = true })
     vim.api.nvim_create_autocmd("BufWritePost", {
